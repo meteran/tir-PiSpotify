7a3e80a68ef3a91cbb2ad042a90b11bc2a9a264c
Piotr Wójcik <piotr_to@op.pl>
2016-05-31 21:50:22 +0200

    playing music

--- spotify.py
+++ spotify.py
@@ -28,6 +28,14 @@ class Spotify(object):
         self.query_count = self.config.query_count
         self.search = None
 
+        self.set_signals()
+
+        try:
+            self.audio_driver = spotify.AlsaSink(self.session)
+        except ImportError:
+            self.logger.warning(
+                'No audio sink found; audio playback unavailable.')
+
     def set_volume(self, percent=None):
         if type(percent) == int and 100 >= percent >= 0:
             self.volume = percent
@@ -43,12 +51,14 @@ class Spotify(object):
         except:
             pass
 
+    @login_required
     def search(self, query=''):
         d = Deferred()
         self.search = self.session.search(query, callback=lambda x: d.callback(x.tracks),
                                           track_count=self.query_count, album_count=0, artist_count=0, playlist_count=0)
         return d
 
+    @login_required
     def more(self):
         assert self.search
         d = Deferred()
@@ -74,6 +84,29 @@ class Spotify(object):
     def stop(self):
         self.session.player.play(False)
         self.session.player.unload()
+
+    @login_required
+    def do_seek(self, seconds):
+        if self.session.player.state is spotify.PlayerState.UNLOADED:
+            self.logger.warning('A track must be loaded before seeking')
+            return
+        self.session.player.seek(int(seconds) * 1000)
+
+    def connection_state_changed(self, session):
+        if session.connection.state is spotify.ConnectionState.LOGGED_IN:
+            self.logged_in = True
+        elif session.connection.state is spotify.ConnectionState.LOGGED_OUT:
+            self.logged_in = False
+
+    def end_of_track(self, _):
+        self.session.player.play(False)
+
+    def set_signals(self):
+        self.session.on(
+            spotify.SessionEvent.CONNECTION_STATE_UPDATED,
+            self.connection_state_changed)
+        self.session.on(
+            spotify.SessionEvent.END_OF_TRACK, self.end_of_track)
 
 
 if __name__ == "__main__":
